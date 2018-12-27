4b84c1dbb0b101574cab59058da68a234048ee4c
Dewey Hong <doohee323@gmail.com>
2018-12-27 08:15:23 -0800

    .

--- scripts/jenkinsx.1.sh
+++ scripts/jenkinsx.1.sh
@@ -42,9 +42,9 @@ echo 'export PATH=$PATH:~/.jx/bin' >> ~/.bashrc
 ###############################################################
 cat > /tmp/login_gcloud.txt <<- "EOF"
 	gcloud auth login
-	#gcloud config set project sodadevops
-	# gcloud init
-	# gcloud components update
+	gcloud config set project tz-devops
+	#gcloud init
+	#gcloud components update
 EOF
 cat /tmp/login_gcloud.txt
 
@@ -52,9 +52,9 @@ cat /tmp/login_gcloud.txt
 # make a jenkinsx in gcp
 ###############################################################
 cat > /tmp/cluster.txt <<- "EOF"
-	#jx create cluster gke -n jxapp4 --username admin --default-admin-password admin123! --verbose=true --log-level debug
-	jx create cluster gke --skip-login=true -n jxapp4 --username admin --default-admin-password admin123! --verbose=true --log-level debug \
-	 -p sodadevops -z northamerica-northeast1-a --machine-type n1-standard-2 --max-num-nodes 1 --min-num-nodes 1
+	#jx create cluster gke -n tzops3 --username admin --default-admin-password admin123! --verbose=true --log-level debug
+	jx create cluster gke -n tzops3 --username admin --default-admin-password admin123! --verbose=true --log-level debug \
+	 -p tz-devops -z northamerica-northeast1-a --machine-type n1-standard-2 --max-num-nodes 3 --min-num-nodes 2
 
 	? Domain 35.203.71.249.nip.io
 	? GitHub user name: doohee323@gmail.com
@@ -64,35 +64,6 @@ cat > /tmp/cluster.txt <<- "EOF"
 	? API Token: ****************************************
 	? Do you wish to use GitHub as the pipelines Git server: Yes
 	? Do you wish to use doohee323@gmail.com as the pipelines Git user for GitHub server: Yes
-
-    ### Delete a cluster:
-	jx delete aws --log-level debug --vpc-id vpc-0ed6e1b6e1200437c jxapp4.cluster.k8s.local 
-	jx delete aws --log-level debug --vpc-id vpc-0ed6e1b6e1200437c aws1.cluster.k8s.local 
-
-	kubectl config get-contexts
-	#kubectl config use-context docker-for-desktop
-	kubectl config use-context aws1.cluster.k8s.local
-
-	vi /Users/dhong/.kube/config
-	kubectl config delete-cluster aws1
-	kubectl config delete-context aws1.cluster.k8s.local
-	kubectl config unset contexts
-
-	https://github.com/jenkins-x/jx/issues/222
-
-	# delete jx-staging (namespace)
-	> jx context
-		gke_sodadevops_northamerica-northeast1-a_jxapp4
-		...
-	> jx namespace
-		jx-staging
-	> jx uninstall
-		gke_sodadevops_northamerica-northeast1-a_jxapp4
-
-	# remove context
-	jx context
-	kubectl config delete-context gke_sodadevops_us-west1-a_jenkinsx-demo
-
 EOF
 cat /tmp/cluster.txt
 
@@ -102,8 +73,15 @@ echo "###[ Take over 5 minutes to be ready for build env.]######################
 # prepare jx
 ###############################################################
 cat > /tmp/prepare_jx.txt <<- "EOF"
-	#jx init
-	jx install --username doohee323
+	$ jx init
+	$ jx install --username doohee323
+	? Do you wish to use doohee323@gmail.com as the local Git user for GitHub server: Yes
+	? Do you wish to use GitHub as the pipelines Git server: Yes
+	? Do you wish to use doohee323@gmail.com as the pipelines Git user for GitHub server: Yes
+	? A local Jenkins X cloud environments repository already exists, recreate with latest? Yes
+	? Select Jenkins installation type: Static Master Jenkins
+	? Pick workload build pack:  Kubernetes Workloads: Automated CI+CD with GitOps Promotion
+	? Select the organization where you want to create the environment repository: Sodacrew	
 EOF
 cat /tmp/prepare_jx.txt
 
@@ -118,16 +96,6 @@ cat > /tmp/check_env.txt <<- "EOF"
     jenkins-x-monocular-ui    http://monocular.jx.35.247.21.77.nip.io
     nexus                     http://nexus.jx.35.247.21.77.nip.io
 
-	$ jx init
-	$ jx install --username doohee323
-	? Do you wish to use doohee323@gmail.com as the local Git user for GitHub server: Yes
-	? Do you wish to use GitHub as the pipelines Git server: Yes
-	? Do you wish to use doohee323@gmail.com as the pipelines Git user for GitHub server: Yes
-	? A local Jenkins X cloud environments repository already exists, recreate with latest? Yes
-	? Select Jenkins installation type: Static Master Jenkins
-	? Pick workload build pack:  Kubernetes Workloads: Automated CI+CD with GitOps Promotion
-	? Select the organization where you want to create the environment repository: Sodacrew	
-
     $ jx env
     ? Pick environment:  [Use arrows to move, type to filter]
     dev
@@ -137,21 +105,15 @@ cat > /tmp/check_env.txt <<- "EOF"
 	$ jx get env
 	NAME       LABEL       KIND        PROMOTE NAMESPACE     ORDER CLUSTER SOURCE                                                                REF PR
 	dev        Development Development Never   jx            0
-	staging    Staging     Permanent   Auto    jx-staging    100           https://github.com/doohee323/environment-jxapp4-staging.git
-	production Production  Permanent   Manual  jx-production 200           https://github.com/doohee323/environment-jxapp4-production.git
+	staging    Staging     Permanent   Auto    jx-staging    100           https://github.com/doohee323/environment-tzops3-staging.git
+	production Production  Permanent   Manual  jx-production 200           https://github.com/doohee323/environment-tzops3-production.git
 	
 	$ jx get apps
 	APPLICATION  STAGING PODS URL                                                PRODUCTION PODS URL
 	demo1001     0.0.3   1/1  http://demo1001.jx-staging.35.247.21.77.nip.io
 	jhapp        0.0.11       http://jhapp.jx-staging.35.247.21.77.nip.io
 	soda-gift    0.0.2   1/1  http://soda-gift.jx-staging.35.247.21.77.nip.io
-	jxapp4 0.0.3        http://jxapp4.jx-staging.35.247.21.77.nip.io
-
-	# set api key
-	rm -Rf ~/.jx/jenkinsAuth.yaml
-	rm -Rf .jx
-	rm -Rf .kube
-	rm -Rf .helm
+	tzops3 0.0.3        http://tzops3.jx-staging.35.247.21.77.nip.io
 EOF
 cat /tmp/check_env.txt
 
@@ -177,15 +139,15 @@ cat > /tmp/check_env.txt <<- "EOF"
 	$ jx get env
 	NAME       LABEL       KIND        PROMOTE NAMESPACE     ORDER CLUSTER SOURCE                                                                REF PR
 	dev        Development Development Never   jx            0
-	staging    Staging     Permanent   Auto    jx-staging    100           https://github.com/doohee323/environment-jxapp4-staging.git
-	production Production  Permanent   Manual  jx-production 200           https://github.com/doohee323/environment-jxapp4-production.git
+	staging    Staging     Permanent   Auto    jx-staging    100           https://github.com/doohee323/environment-tzops3-staging.git
+	production Production  Permanent   Manual  jx-production 200           https://github.com/doohee323/environment-tzops3-production.git
 	
 	$ jx get apps
 	APPLICATION  STAGING PODS URL                                                PRODUCTION PODS URL
 	demo1001     0.0.3   1/1  http://demo1001.jx-staging.35.247.21.77.nip.io
 	jhapp        0.0.11       http://jhapp.jx-staging.35.247.21.77.nip.io
 	soda-gift    0.0.2   1/1  http://soda-gift.jx-staging.35.247.21.77.nip.io
-	jxapp4 0.0.3        http://jxapp4.jx-staging.35.247.21.77.nip.io
+	tzops3 0.0.3        http://tzops3.jx-staging.35.247.21.77.nip.io
 EOF
 cat /tmp/check_env.txt
 
